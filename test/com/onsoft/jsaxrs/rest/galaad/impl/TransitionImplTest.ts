e7b1629ed6c9e8f7649ccfe5768790412d218896
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-04 12:01:48 +0200

    Test update
    Add test suite for the TransitionImpl class.

---
+++ test/com/onsoft/jsaxrs/rest/galaad/impl/TransitionImplTest.ts
@@ -0,0 +1,32 @@
+import 'mocha';
+import { expect } from 'chai';
+
+// Class to test:
+import { TransitionImpl } from '../../../../../../../src/com/onsoft/jsaxrs/rest/galaad/impl/TransitionImpl';
+import { Transition } from '../../../../../../../src/com/onsoft/jsaxrs/rest/hateoas/Transition';
+
+// Utilities:
+
+// Test:
+describe('@TransitionImpl class', () => {
+
+    it('@TransitionImpl should have a type property initialized to null', () => {
+        const transition: Transition = new TransitionImpl();
+        expect(transition.type).to.be.null;
+    });
+
+    it('@TransitionImpl should have a resource property initialized to null', () => {
+        const transition: Transition = new TransitionImpl();
+        expect(transition.resource).to.be.null;
+    });
+    
+    it('@TransitionImpl should have a method property initialized to null', () => {
+        const transition: Transition = new TransitionImpl();
+        expect(transition.method).to.be.null;
+    });
+    
+    it('@TransitionImpl should have a rel property initialized to null', () => {
+        const transition: Transition = new TransitionImpl();
+        expect(transition.rel).to.be.null;
+    });
+});
