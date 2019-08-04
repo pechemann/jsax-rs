742e4d8dda26ed613164b17d6de1b8a7cfecd182
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-04 12:08:05 +0200

    Test update
    Add test suite for the StateImpl class.

---
+++ test/com/onsoft/jsaxrs/rest/galaad/impl/StateImplTest.ts
@@ -0,0 +1,37 @@
+import 'mocha';
+import { expect } from 'chai';
+
+// Class to test:
+import { StateImpl } from '../../../../../../../src/com/onsoft/jsaxrs/rest/galaad/impl/StateImpl';
+import { State } from '../../../../../../../src/com/onsoft/jsaxrs/rest/hateoas/State';
+
+// Utilities:
+
+// Test:
+describe('@StateImpl class', () => {
+
+    it('@StateImpl should have a name property initialized to null', () => {
+        const state: State = new StateImpl();
+        expect(state.name).to.be.null;
+    });
+
+    it('@StateImpl should have a type property initialized to null', () => {
+        const state: State = new StateImpl();
+        expect(state.type).to.be.null;
+    });
+
+    it('@StateImpl should have a resource property initialized to null', () => {
+        const state: State = new StateImpl();
+        expect(state.resource).to.be.null;
+    });
+    
+    it('@StateImpl should have a method property initialized to null', () => {
+        const state: State = new StateImpl();
+        expect(state.method).to.be.null;
+    });
+    
+    it('@StateImpl should have a transitions property initialized to null', () => {
+        const state: State = new StateImpl();
+        expect(state.transitions).to.be.null;
+    });
+});
